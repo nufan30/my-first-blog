7585cb4dbe5bd9b161a981de3200fd21c4754890
nufan30 <areyes30@gmail.com>
2025-03-11 15:40:39 -0400

    Changed allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+mrkx6*m9&6rb)v8=^t_*)(_n1vy_v6%74tef88q(^^8ovf#+1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'areyes30.pythonanywhere.com']
 
 
 # Application definition
